4990c030b3056352ea87a988062cd618a07c125c
Jaivardhan <jvss1996@gmail.com>
2024-03-03 15:55:56 -0600

    created docker compose file, configure database connection

---
+++ src/main/java/com/shekhawat/onechat/OnechatApplication.java
@@ -0,0 +1,13 @@
+package com.shekhawat.onechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnechatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shekhawat/onechat/OnechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shekhawat.onechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
